8290b9b8497381feb69731f169abb5e3eb3a6774
maissaAmg <109427534+maissaAmg@users.noreply.github.com>
2024-04-30 11:42:58 +0100

    TextToSpeech

--- app/src/main/java/com/codingwithmitch/essaimodel/MainActivity.kt
+++ app/src/main/java/com/codingwithmitch/essaimodel/MainActivity.kt
@@ -4,16 +4,22 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     private lateinit var adlEditText: EditText
     private lateinit var fallEditText: EditText
+
+    private lateinit var textToSpeech: TextToSpeech
+
 
     // Déclaration d'un BroadcastReceiver pour recevoir les résultats de l'inférence.
     private lateinit var resultReceiver: BroadcastReceiver
@@ -21,6 +27,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textToSpeech = TextToSpeech(this, this)
+        textToSpeech.language = Locale.US
 
 
         adlEditText = findViewById(R.id.ADL)
@@ -48,17 +57,47 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // TextToSpeech initialization successful
+        } else {
+            // TextToSpeech initialization failed
+        }
+    }
+
+
     // Méthode pour mettre à jour l'interface utilisateur avec les résultats de l'inférence.
     private fun updateUIWithResults(result: FloatArray) {
         if (result.size >= 2) {
-            adlEditText.setText(result[0].toString())
-            fallEditText.setText(result[1].toString())
+
+            val adlValue = result[0]
+            val fallValue = result[1]
+
+            adlEditText.setText(adlValue.toString())
+            fallEditText.setText(fallValue.toString())
+
+
+            // Check if fall value is greater than ADL value
+            if (fallValue > adlValue) {
+                // Trigger voice prompt here
+                Log.d("*******","************************")
+                Log.d("VoiceTrigger", "A fall has occured")
+                Log.d("*******","************************")
+                speak("Fall detected")
+            }
         }
     }
+
+    private fun speak(text: String) {
+        textToSpeech.speak(text, TextToSpeech.QUEUE_ADD, null, null)
+    }
+
 
     // Méthode onDestroy appelée lors de la destruction de l'activité.
     override fun onDestroy() {
         super.onDestroy()
+        textToSpeech.stop()
+        textToSpeech.shutdown()
         // Unregister the BroadcastReceiver
         LocalBroadcastManager.getInstance(this).unregisterReceiver(resultReceiver)
     }
